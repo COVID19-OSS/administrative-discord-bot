4b5570d0a9c0dc44856f0012620eb6f6f0f88ae2
BrightSkyz <BrightSkyz@users.noreply.github.com>
2020-03-20 19:37:21 -0400

    Update StuckChannelDiscordCommand.ts

--- src/command/punishment/StuckChannelDiscordCommand.ts
+++ src/command/punishment/StuckChannelDiscordCommand.ts
@@ -3,15 +3,17 @@ import {MessageEmbed, TextChannel} from "discord.js";
 
 const { QUARANTINE_ROLES } = process.env;
 
+const AUTHORIZED_ROLES = QUARANTINE_ROLES ? QUARANTINE_ROLES.split(",").map((s: string) => s.trim()) : [ "Administrator" ];
+
 export class StuckChannelDiscordCommand extends DiscordCommand {
-  static readonly AUTHORIZED_ROLES = QUARANTINE_ROLES!.split(",").map((s: string) => s.trim()) || [ "Administrator" ];
 
   public async execute(): Promise<void> {
     await this.message.channel.delete("Cleanup quarantined channel.");
   }
 
   public async validate(): Promise<boolean> {
-    if (!this.message.member?.roles.cache.some(role => StuckChannelDiscordCommand.AUTHORIZED_ROLES.includes(role.name))) {
+    /* Role check */
+    if (!this.message.member?.roles.cache.some(role => AUTHORIZED_ROLES.includes(role.name))) {
       console.error("Stuck channel command used by user who does not have permission!");
       return false;
     }
