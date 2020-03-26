e677f699f5804d2e9497a0f8f830334e3717a28c
COVID-GUH <guh1337@protonmail.ch>
2020-03-26 00:15:15 -0400

    error handle

--- src/event/CloseVerificationAttemptEventListener.ts
+++ src/event/CloseVerificationAttemptEventListener.ts
@@ -16,20 +16,26 @@ export class CloseVerificationAttemptEventListener extends EventListener {
 
   @bind
   private async handleCloseVerificationAttempt(message: Message): Promise<void> {
-    if (message.channel.id !== VERIFICATION_CHANNEL_ID) return;
-    if (message.member?.roles.cache.some(role => role.name === VERIFIED_ROLE)) return;
-    const missingSpace = message.content.startsWith(DISCORD_PREFIX!) && message.content.split(" ").length !== 2;
-    if (message.content.startsWith(DiscordCommandType.VERIFY) || missingSpace) {
-      const description = `Hey there ${message.author} we noticed that you might be having some difficulty verifying your account.\n\n`
-        + "You might need to add the `>` symbol in front of your message or have a space between the `>command code` so our system can pick it up correctly. We look forward to you joining our community!\n\n"
-        + "Please refrain from contacting our staff if you have not read the rules, it might result in your removal from our community";
-      const embed = new MessageEmbed()
-        .setTitle("COVID19 Community Discord - Verification")
-        .setDescription(description)
-        .setThumbnail("https://i.imgur.com/UyieFtd.png")
-        .setColor("#d4443f")
-        .setFooter("I'm a robot. Beep boop.");
-      await message.author.send(embed);
+    try {
+      if (message.channel.id !== VERIFICATION_CHANNEL_ID) return;
+      if (message.member?.roles.cache.some(role => role.name === VERIFIED_ROLE)) return;
+      const missingSpace = message.content.startsWith(DISCORD_PREFIX!) && message.content.split(" ").length !== 2;
+      if (message.content.startsWith(DiscordCommandType.VERIFY) || missingSpace) {
+        const description = `Hey there ${message.author} we noticed that you might be having some difficulty verifying your account.\n\n`
+          + "You might need to add the `>` symbol in front of your message or have a space between the `>command code` so our system can pick it up correctly. We look forward to you joining our community!\n\n"
+          + "Please refrain from contacting our staff if you have not read the rules, it might result in your removal from our community";
+        const embed = new MessageEmbed()
+          .setTitle("COVID19 Community Discord - Verification")
+          .setDescription(description)
+          .setThumbnail("https://i.imgur.com/UyieFtd.png")
+          .setColor("#d4443f")
+          .setFooter("I'm a robot. Beep boop.");
+        await message.author.send(embed).catch(() => undefined);
+      }
+    }
+    catch (error) {
+      console.error("Could not handle close verification attempt");
+      console.error(error);
     }
   }
 }
