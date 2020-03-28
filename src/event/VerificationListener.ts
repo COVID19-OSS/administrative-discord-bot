703628f5576e8289fee486242f48f56f8dd0a5ef
GUH <62044207+COVID-GUH@users.noreply.github.com>
2020-03-28 15:13:40 -0400

    new rules, optional prefix, more close verification handlers (#29)

---
+++ src/event/VerificationListener.ts
@@ -0,0 +1,28 @@
+import bind from "bind-decorator";
+import { Message } from "discord.js";
+
+import { EventListener } from "./EventListener";
+import { DiscordCommandType } from "../command/DiscordCommandType";
+import { DiscordCommandRegistry } from "../command/DiscordCommandRegistry";
+import { ListenerDependencies } from "../definitions/dependencies/ListenerDependencies";
+
+export class VerificationListener extends EventListener {
+  constructor(dependencies: ListenerDependencies) {
+    super(dependencies);
+    this.dependencies.discordService.bindMessageListener(this.handleVerificationMessage);
+  }
+
+  @bind
+  private async handleVerificationMessage(message: Message): Promise<void> {
+    if (message.content.startsWith(DiscordCommandType.VERIFY)) {
+      const args = message.content.split(" ").slice(1);
+      const commandExecutor = DiscordCommandRegistry.getCommand(DiscordCommandType.VERIFY, args, message, this.dependencies);
+
+      const valid = await commandExecutor?.validate();
+      if (!valid) return;
+
+      await commandExecutor?.execute();
+    }
+
+  }
+}
