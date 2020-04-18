1dd68d79e1d768c5278559f7982e7110efa9920f
BrightSkyz <BrightSkyz@users.noreply.github.com>
2020-04-18 12:32:26 -0400

    Fix duplicate

--- src/data/AllowedNewsSites.ts
+++ src/data/AllowedNewsSites.ts
@@ -31,10 +31,6 @@ export const AllowedNewsSites = [
         hostnames: ["reuters.com"]
       },
       {
-        label: "Reuters",
-        hostnames: ["reuters.com"]
-      },
-      {
         label: "Business Insider",
         hostnames: ["businessinsider.com"]
       },
